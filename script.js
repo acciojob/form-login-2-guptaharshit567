3f4e9ce5a09b8c28f4ed3c06ebd80ad0d5ab6acc
guptaharshit567 <148793121+guptaharshit567@users.noreply.github.com>
2024-03-09 21:13:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+  document.getElementById("myform").addEventListener("submit",function(event) {
+  	event.preventDefault();
+
+		  const FirstName = this.elements["First Name"].value;
+	  const LastName = this.elements["Last Name"].value;
+	  const Phone = this.elements["Phone Number"].value;
+	  const Email = this.elements["Email ID"].value;
+	  const message = `First Name: ${FirstName}\n Last Name: ${LastName}\n Phone Number: ${Phone}\n Email ID: ${Email}\n`;
+	  alert(message);
+	  
+  });
+
